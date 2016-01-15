0e419f1f91ceba5923df329690f1e2a38bd01da6
James Johnson <james.johnson@excellentingenuity.com>
2016-01-15 11:24:18 -0500

    Added some documentation

--- src/Rule.php
+++ src/Rule.php
@@ -5,5 +5,10 @@ namespace Eig\Ruled;
 
 class Rule
 {
+    protected $chain;
 
+    public function __construct (Chain $chain)
+    {
+        $this->chain = $chain;
+    }
 }
--- tests/RuleTest.php
+++ tests/RuleTest.php
@@ -3,11 +3,14 @@
 namespace Eig\Ruled\Tests;
 
 use Eig\Ruled\Rule;
+use Eig\Ruled\Chain;
 
 class RuleTest extends TestBaseClass
 {
     public function testInstantiation() {
-        $rule = new Rule();
+        $rule = new Rule(new Chain()I wI );
         $this->assertInstanceOf('Eig\Ruled\Rule', $rule);
     }
+
+
 }
